f352c9fe87c50217facf4a381048c17211c52ff9
daniil <klimchuk.daniil@gmail.com>
2018-04-03 15:36:54 +0300

    Add tests for module releasing

---
+++ src/test/kotlin/com/diklimchuk/kotlinJsDi/module/DiModuleReleasingTest.kt
@@ -0,0 +1,36 @@
+package com.diklimchuk.kotlinJsDi.module
+
+import com.diklimchuk.kotlinJsDi.DiComponent
+import com.diklimchuk.kotlinJsDi.DiKey
+import com.diklimchuk.kotlinJsDi.scope.DiScope
+import sinon.Sinon
+import testClasses.TestClass
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class DiModuleReleasingTest {
+
+    @Test
+    fun providerIsSuccesfullyReleased() {
+        val module = createDiModule {
+            it scopes { TestClass() }
+        }
+        val provider = module.getProvider<TestClass>(DiKey.ofClass<TestClass>())
+        val spy = Sinon.spy(provider, "release")
+        module.release()
+        assertTrue(spy.calledOnce)
+    }
+
+    @Test
+    fun subcomponentIsSuccessfullyReleased() {
+        val module = createDiModule {
+            it hasSubcomponents {
+                it add DiComponent() scoped DiScope.SINGLETON
+            }
+        }
+        val subcomponent = module.getSubcomponent(DiScope.SINGLETON)
+        val spy = Sinon.spy(subcomponent, "release")
+        module.release()
+        assertTrue(spy.calledOnce)
+    }
+}
