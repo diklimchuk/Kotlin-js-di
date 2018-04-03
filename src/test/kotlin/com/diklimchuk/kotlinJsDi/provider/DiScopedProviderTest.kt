df77e2f7088086d29c66e802fed440fecfcfe4cc
daniil <klimchuk.daniil@gmail.com>
2018-04-03 04:50:47 +0300

    Add tests for scoped provider

--- src/test/kotlin/com/diklimchuk/kotlinJsDi/provider/DiScopedProviderTest.kt
+++ src/test/kotlin/com/diklimchuk/kotlinJsDi/provider/DiScopedProviderTest.kt
@@ -2,15 +2,52 @@ package com.diklimchuk.kotlinJsDi.provider
 
 import com.diklimchuk.kotlinJsDi.DiComponent
 import runTest
+import testClasses.IntWrapperTestClass
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotSame
+import kotlin.test.assertSame
 
 class DiScopedProviderTest {
 
     @Test
     fun providerWillCallFactory() = runTest {
+        val result = DiScopedProvider({ IntWrapperTestClass(47) }).provide(DiComponent())
+        assertEquals(IntWrapperTestClass(47), result)
+    }
+
+    @Test
+    fun providerReturnsCachedInstanceOnce() = runTest {
         class SimpleClass
 
-        val factory: suspend DiComponent.() -> SimpleClass = { SimpleClass() }
-        DiScopedProvider(factory).provide(DiComponent())
+        val component = DiComponent()
+        val provider = DiScopedProvider({ SimpleClass() })
+        val firstEmission = provider.provide(component)
+        val secondEmission = provider.provide(component)
+        assertSame(firstEmission, secondEmission)
+    }
+
+    @Test
+    fun providerReturnsCachedInstanceTwice() = runTest {
+        class SimpleClass
+
+        val component = DiComponent()
+        val provider = DiScopedProvider({ SimpleClass() })
+        val firstEmission = provider.provide(component)
+        provider.provide(component)
+        val thirdEmission = provider.provide(component)
+        assertSame(firstEmission, thirdEmission)
+    }
+
+    @Test
+    fun providerReturnsDifferentInstanceAfterRelease() = runTest {
+        class SimpleClass
+
+        val component = DiComponent()
+        val provider = DiScopedProvider({ SimpleClass() })
+        val emissionBeforeRelease = provider.provide(component)
+        provider.release()
+        val emissionAfterRelease = provider.provide(component)
+        assertNotSame(emissionBeforeRelease, emissionAfterRelease)
     }
 }
---
+++ src/test/kotlin/testClasses/IntWrapperTestClass.kt
@@ -0,0 +1,13 @@
+package testClasses
+
+class IntWrapperTestClass(
+        private val value: Int
+) {
+    override fun equals(other: Any?): Boolean {
+        return other is IntWrapperTestClass && value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value
+    }
+}
